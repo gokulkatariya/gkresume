305fa870ac2732f4d8a3c652d50c31087a479c62
gokulkatariya <gk2800598@gmail.com>
2023-08-28 10:47:57 +0530

    update

--- src/Components/About.js
+++ src/Components/About.js
@@ -28,7 +28,7 @@ class About extends Component {
               />
             </div>
             <div className="nine columns main-col">
-              <h2>AboutMe</h2>
+              <h2>About Me</h2>
 
               <p>{bio}</p>
               <div className="row">
